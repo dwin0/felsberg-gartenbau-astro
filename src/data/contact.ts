9c318a0c5434398f1d2d6260551dd4fd7cb9f114
David <david.windler@windowslive.com>
2024-03-23 21:43:01 +0100

    add "open positions" page

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -4,4 +4,11 @@ export const zipCode = "8566";
 export const city = "Neuwilen";
 export const googleMapsLink = "https://goo.gl/maps/hgvNTrwkDu52";
 export const phoneNumber = "071 699 21 10";
+export const phoneNumberIntl = `+41${phoneNumber
+  .substring(1)
+  .replaceAll(" ", "")}`;
+export const mobileNumber = "076 330 12 10";
+export const mobileNumberIntl = `+41${mobileNumber
+  .substring(1)
+  .replaceAll(" ", "")}`;
 export const email = "info@felsberg-gartenbau.ch";
