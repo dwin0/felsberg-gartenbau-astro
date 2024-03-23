713e17fc2e4ba6ed26c6fb9be7da20eb0aa66952
David <david.windler@windowslive.com>
2024-03-23 19:36:08 +0100

    add "about us" page

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,3 +1,7 @@
+export const name = "Gartenbau Felsberg GmbH";
+export const street = "Greestrasse 26";
+export const zipCode = "8566";
+export const city = "Neuwilen";
 export const googleMapsLink = "https://goo.gl/maps/hgvNTrwkDu52";
 export const phoneNumber = "071 699 21 10";
 export const email = "info@felsberg-gartenbau.ch";
