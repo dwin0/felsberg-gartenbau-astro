0c258d00feb9fbf48f1e628188e7a22a9697c493
David <david.windler@windowslive.com>
2024-04-01 17:01:07 +0200

    add webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    webmanifest({
+      name: "Gartenbau Felsberg GmbH",
+      short_name: "Gartenbau Felsberg",
+      icon: "src/images/felsberg-icon-round.png",
+      start_url: "/",
+      theme_color: "#245d24",
+      background_color: "#245d24",
+      display: "standalone",
+      lang: "de",
+      dir: "ltr",
+      id: "gartenbau-felsberg",
+      config: {
+        insertAppleTouchLinks: true,
+      },
+    }),
+  ],
+});
